154f9f1011797243d033cfc5358391f47aa4d07f
Madhuvarshini Muthusamy <madhumuthusamy06@gmail.com>
2025-08-25 21:16:20 +0530

    Time: 13 ms (48.58%) | Memory: 19.9 MB (42.00%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -4,15 +4,15 @@ class Solution:
         cols=len(mat[0])
         result=[]
         for diag in range(rows+cols-1):
-            diagonalele=[]
+            diagele=[]
             row=0 if diag<cols else diag-cols+1
             col=diag if diag<cols else cols-1
             while row<rows and col>=0:
-                diagonalele.append(mat[row][col])
+                diagele.append(mat[row][col])
                 row+=1
                 col-=1
             if diag%2==0:
-                result.extend(reversed(diagonalele))
+                result.extend(reversed(diagele))
             else:
-                result.extend(diagonalele)
+                result.extend(diagele)
         return result
